beb121b99c6de323fa6e7fb4aed2ffbf3d3e2869
L00113360 <33516120+L00113360@users.noreply.github.com>
2018-08-12 19:44:55 +0100

    added core files

---
+++ header.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>LYIT - University</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="LYIT - University">
+	<meta name="keywords" content="event, lyit, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->   
+    <link href="img/favicon.png" rel="shortcut icon"/>
+    
+    <!-- Social Image -->
+    <meta property="og:image" content="http://link192.net/lyituni/img/cover1.jpg">
+    <meta property="og:image:type" content="image/jpg">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/themify-icons.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- header section -->
+	<header class="header-section">
+		<div class="container">
+			<!-- logo -->
+			<a href="index.php" class="site-logo"><img src="img/logo.png" alt=""></a>
+			<div class="nav-switch">
+				<i class="fa fa-bars"></i>
+			</div>
+			<div class="header-info">
+				<div class="hf-item">
+					<i class="fa fa-clock-o"></i>
+					<p><span>Library Hours:</span>Monday - Friday: 08 AM - 06 PM</p>
+				</div>
+				<div class="hf-item">
+					<i class="fa fa-map-marker"></i>
+					<p><span>Find us:</span>Port Road, Letterkenny, Donegal</p>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- header section end-->
+
+
+	<!-- Header section  -->
+	<nav class="nav-section">
+		<div class="container">
+			<div class="nav-right">
+				<a href=""><i class="fa fa-user"></i> Login</a>
+				<a href=""><i class="fa fa-edit"></i> Register</a>
+			</div>
+			<ul class="main-menu">
+				<li class="active"><a href="#">Home</a></li>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">COURSES</a></li>
+				<li><a href="#">blog</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</div>
+	</nav>
+	<!-- Header section end -->
