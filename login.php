805bbfb7c3fb6a7d6304741f85b7b2fd0bfcd826
L00113360 <33516120+L00113360@users.noreply.github.com>
2018-08-12 20:59:05 +0100

    Just ensuring all is correct - something didnt look right according to my VScode

---
+++ login.php
@@ -0,0 +1,22 @@
+<!-- Header -->
+<?php include ('header.php'); ?>
+<!-- Header End - Do Not Remove -->
+
+<!-- Your Content Start-->
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Your Content End-->
+
+<!-- Footer -->
+<?php include ('footer.php'); ?>
+<!-- Header End - Do Not Remove -->
