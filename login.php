bb38c4da3bac8fcbd047edb2751d4c89aafb9071
ConorDorrian1 <L00101441@student.lyit.ie>
2018-08-13 14:13:56 +0100

    Login page for website

--- login.php
+++ login.php
@@ -3,17 +3,111 @@
 <!-- Header End - Do Not Remove -->
 
 <!-- Your Content Start-->
+<!--<style>
+body{
+    margin:0;
+    padding: 0; 
+    font-family: sans-serif;
+}
+
+.loginbox{
+    width: 320px;
+    height: 430px;
+    background: #000;
+    color: #fff;
+    top: 50%;
+    left: 50%;
+    position: absolute;
+    transform: translate(-50%,-50%);
+    box-sizing: border-box;
+    padding: 70px 30px;
+    border-radius: 19px;
+}
+.avatar{
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    position: absolute;
+    top: -10%;
+    left: calc(50% - 50px);
+}
+
+h1{
+    margin: 0;
+    padding: 0 0 20px;
+    text-align: center;
+    font-size: 22px;
+}
+
+.loginbox p{
+    margin: 0;
+    padding: 0;
+    font-weight: bold;
+}
 
 
+.loginbox input{
+    width: 100%;
+    margin-bottom: 20px;
+}
 
+.loginbox input[type = "text"], input[type = "password"]{
+    border: none;
+    border-bottom: 1px solid #fff;
+    background: transparent;
+    outline: none;
+    height: 40px;
+    color: #fff;
+    font-size: 16px;
+}
 
+.loginbox input[type = "submit"]{
+    border: none;
+    outline: none;
+    height: 40px;
+    background: #fb2525;
+    color: #fff;
+    font-size: 18px;
+    border-radius: 20px;
+}
 
-<h1>Coming Soon</h1>
+.loginbox input[type = "submit"]:hover{
+    cursor: pointer;
+    background: #ffc107;
+    color: #000;
+}
 
+.loginbox a{
+    text-decoration: none;
+    font-size: 12px;
+    line-height: 20px;
+    color: darkgrey;
+}
 
-
-
-
+.loginbox a:hover{
+    color: #ffc107;
+}
+</style>-->
+<section>
+    <div class="comment-form container">
+            <img src="003aa.png" alt="" class = avatar>
+            <h1>Login Here</h1>
+            <form>
+                <p>Username</p>
+                <input type="text" name="" placeholder = "Enter username">
+                <p>Password</p>
+                <input type="text"  name="" placeholder = "Enter password">
+                <br>
+                
+                
+                <input type="submit" name="" value="Login">
+                <br>
+                <a href="#">Lost your Password?</a>
+                <br>
+                <a href="register.php">Dont have an account?</a>
+            </form>
+        </div>
+</section>
 
 <!-- Your Content End-->
 
