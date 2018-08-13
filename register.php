4eb2090c1927c028ae6f07706dac8579345f3535
ConorDorrian1 <L00101441@student.lyit.ie>
2018-08-13 17:13:06 +0100

    Making register page feel better. Still need to sort Submit button to go back to login page

--- register.php
+++ register.php
@@ -4,57 +4,70 @@
 
 <!-- Your Content Start-->
 
-<section>
-<form>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+<section class = 'container'>
+    <form>
+    <br>
+    <div class="form-row">
+        <div class="form-group col-md-6">
+        <label for="inputUser">Username</label>
+        <input type="textarea" class="form-control" id="inputUser" placeholder="Username">
+        <br>
+        </div>
+        <div class="form-group col-md-6">
+    
+        <label for="inputPassword4">Password</label>
+        <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+        &#160;&nbsp;
+        <input class="form-check-input" type="checkbox" id="gridCheck">
+        <label class="form-check-label" for="gridCheck">
+            Show Password
+        </div>
+        <div class="form-group col-md-6">
+        <label for="inputEmail4">Email</label>
+        <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+        </div>
     </div>
-    <div class="form-group col-md-6">
-      <label for="inputPassword4">Password</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-      <label class="form-check-label" for="gridCheck">
-        Show Password
+    <div class="form-group">
+        <label for="inputAddress">Address</label>
+        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
     </div>
-  </div>
-  <div class="form-group">
-    <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
-      <input type="text" class="form-control" id="inputCity">
+    <div class="form-group">
+        <label for="inputAddress2">Address 2</label>
+        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
     </div>
-    <div class="form-group col-md-4">
-      <label for="inputState">State</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
+    <div class="form-row">
+        <div class="form-group col-md-6">
+        <label for="inputCity">City</label>
+        <input type="text" class="form-control" id="inputCity">
+        </div>
+        <div class="form-group col-md-4">
+        <label for="inputState">State</label>
+        <select id="inputState" class="form-control">
+            <option selected>Choose...</option>
+            <option>Ulster</option>
+            <option>Leinster</option>
+            <option>Connacht</option>
+            <option>Munster</option>
+        </select>
+        </div>
+        <div class="form-group col-md-2">
+        <label for="inputZip">Zip</label>
+        <input type="text" class="form-control" id="inputZip">
+        </div>
     </div>
-    <div class="form-group col-md-2">
-      <label for="inputZip">Zip</label>
-      <input type="text" class="form-control" id="inputZip">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign in</button>
-</form>
+    <div class="form-group">
+        <div class="form-check">
+        
+        </label>
+        </div>
+        
+   <!-- <button type="submit" class="btn btn-primary" href = "login.php" >Submit</button>-->
+    </form>
+    <form>
+         <input onClick="window.location.href='http://localhost/01_LyitUni/login.php'" type="submit" Value="Submit">
+      </form>
  </section>
-
+<br>
 
 <!-- Your Content End-->
 
