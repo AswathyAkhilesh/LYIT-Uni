00b97493ae55d50521aa18dabea12825b4e7b986
unknown <L00149517@student.lyit.ie>
2018-08-13 22:15:04 +0100

    Added course detail page html

--- course.php
+++ course.php
@@ -2,21 +2,81 @@
 <?php include ('header.php'); ?>
 <!-- Header End - Do Not Remove -->
 
-<!-- Your Content Start-->
-
-
-
-
-
-<h1>Coming Soon</h1>
-
-
-
-
-
-
-<!-- Your Content End-->
-
+      <div class="container">
+         <div class="jumbotron" style="background-image: url('img1.jpg');color:white">
+            <h1>Master of Science in DevOps </h1>
+            <h2>
+            Course Essentials
+            <h2>
+            <p>DevOps, a new organizational and cultural way of organizing development and IT operations work, is spreading rapidly – driven by mounting evidence of its benefits to the business. But reaping these gains requires rethinking application security to deliver more secure code at DevOps speed.</p>
+         </div>
+         <h3 class="card-title" style="color:#22206f">Key Information </h3>
+         <div class="row">
+            <div class="col-sm-4" style="background-color:lavenderblush;">Award :-Masters</div>
+            <div class="col-sm-4" style="background-color:lavenderblush;">Duration :- 1 and 2 year</div>
+            <div class="col-sm-4" style="background-color:lavenderblush;">Course code:- COOD1</div>
+            <div class="col-sm-4" style="background-color:lavenderblush;">Credit :- 30</div>
+            <div class="col-sm-4" style="background-color:lavenderblush;">Study Mode :- Full-time and Part-time</div>
+            <div class="col-sm-4" style="background-color:lavenderblush;">Application Deadline :- 24 august</div>
+         </div>
+      </div>
+      </div>
+      <div class="container course-details">
+         <br><br>
+         <ul class="nav nav-tabs">
+            <li class="active"><a href="#a"aria-controls="home" role="tab" data-toggle="tab">COURSE DETAILS AND MODULES</a></li>
+            <li><a href="#b"aria-controls="home" role="tab" data-toggle="tab" >ENTRY REQUIREMENTS</a></li>
+            <li><a href="#c"aria-controls="home" role="tab" data-toggle="tab">COURSE FEE</a></li>
+            <li><a href="#d"aria-controls="home" role="tab" data-toggle="tab">HOW TO APPLY</a></li>
+            <li><a href="#e"aria-controls="home" role="tab" data-toggle="tab">JOBS</a></li>
+         </ul>
+         <!-- Tab panes -->
+         <div class="tab-content">
+            <div role="tabpanel" class="tab-pane active" id="a"><br><br><b>Our Master of Science in Computing in DevOps is a one year, full-time or two year, part-time programme. DevOps is the practice of operations and development engineers participating together in the entire service lifestyle, from design through the development process to production support.
+               DevOps is characterised by operations staff harnessing the same techniques as developers for their systems work. Topics the course covers include: DevOps Software Engineering, Project Management and Deployment Pipelines.</b><br><br> Proposed Module for this year are as Follows:-  <br><br> DevOps Software Engineering -	Mandatory<br><br>OO Programming for Server Administration-Mandatory<br><br>Scripting the Deployment Pipeline-Mandatory<br><br>DevOps Project Management-Mandatory<br><br>Dissertation-Mandatory
+            </div>
+            <div role="tabpanel" class="tab-pane" id="b">
+               <b><br>Minimum Entry Requirements</b><br><br>
+               - Level 8 Honours Degree in Computing, or equivalent, second class honours (2.2), or Higher Diploma in Computing (Conversion Course into Computing).<br> 
+               -If you do not have an honours degree but have relevant experience you may also be eligible to apply via Recognition of Prior Learning (RPL).  
+               <br><br>
+               <table class="cao-table">
+                  <tbody>
+                     <tr>
+                        <th>Number of Places</th>
+                        <th>15</th>
+                     </tr>
+                  </tbody>
+               </table>
+            </div>
+            <div role="tabpanel" class="tab-pane" id="c" >
+               <br><b>The EU and NON EU citizens have got different fee structure</b> 
+               <h6 style="color:#22206f">EU Fees: €5,000.00</h6>
+               <h6 style="color:#22206f">Non-EU Fees: €9,000.00</h6>
+            </div>
+            <div role="tabpanel" class="tab-pane" id="d"><br>Depending on what type of applicant you are, the application process will differ.
+               For applying to this course please click on the APPLY button.<br> <br>
+               <button type="button" class="btn btn-success" style="text-align:center">APPLY</button>
+            </div>
+            <div role="tabpanel" class="tab-pane" id="e">
+               <h3>Career Pathways</h3>
+               <p>The main employers are:</p>
+               <ul>
+                  <li>Software Companies</li>
+                  <li>Multinational Companies</li>
+               </ul>
+               <h3>Graduate Careers</h3>
+               <p>Former graduates are employed in the following capacities:</p>
+               <ul>
+                  <li>DevOps Engineer</li>
+                  <li>Cloud DevOps Engineer</li>
+                  <li>Systems Software Engineer</li>
+               </ul>
+            </div>
+         </div>
+      </div>
+      <br>
+      </div>
 <!-- Footer -->
 <?php include ('footer.php'); ?>
 <!-- Header End - Do Not Remove -->
