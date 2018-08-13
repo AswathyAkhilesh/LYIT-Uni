66e0c578886324e52f9434bc2710deb0ae4ec902
ConorDorrian1 <L00101441@student.lyit.ie>
2018-08-13 14:17:59 +0100

    Registration page website.

--- register.php
+++ register.php
@@ -4,9 +4,102 @@
 
 <!-- Your Content Start-->
 
-
-
-
+<section>
+    <div class="Regbox comment-form container">
+            <h1>Register Here</h1>
+            <form>
+                <table>
+                    <tr>
+                        <td> 
+                        Name:   
+                        </td>
+                        <td>
+                            <input type="textbox"><br><br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Username:
+                        </td>
+                        <td>
+                            <input type="textbox"><br><br>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>
+                            Password:
+                        </td>
+                        <td>
+                            <input type="password"><br><br>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>
+                            Email:
+                        </td>
+                        <td>
+                            <input type="textbox" ><br><br>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>
+                            Gender:
+                        </td>
+                        <td>
+                            <input type="radio" name = "Gender">Male
+                             <input type="radio" name = "Gender">female<br><br>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>
+                            Date of of birth:
+                        </td>
+                        <td>
+                            <input type="date"><br><br>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>
+                            Address:
+                        </td>
+                        <td>
+                            <input type="textarea" rows="3"><br><br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Town/City::
+                        </td>
+                        <td>
+                            <input type="textbox"><br><br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            County:
+                        </td>
+                        <td>
+                            <input type="textbox"><br><br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Country:
+                        </td>
+                        <td>
+                            <input type="textbox"><br><br>
+                        </td>
+                    </tr>
+                     <tr>
+                        <td>
+                            <input type="submit" value = "Submit">
+                        </td>
+                    </tr>
+                    
+                </table>
+            </form>
+        </div>
+ </section>
 
 
 
