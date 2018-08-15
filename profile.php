900d32bef3ee1ac367813f4647bbcfdfc26df951
L00130494 <L00130494@student.lyit.ie>
2018-08-15 17:09:47 +0100

    the final profile page

--- profile.php
+++ profile.php
@@ -15,18 +15,23 @@ body { height: 100%; background-color: #FFFFFF; font: 1.2em Verdana, Arial, Helv
 
 /* ==================== Form style sheet ==================== */
 
-form { margin: 25px 0 0 29px; width: 450px; padding-bottom: 30px; }
+form { margin: 25px 0 0 29px; width: 750px; padding-bottom: 30px; padding-left: 200px;}
 
 fieldset { margin: 0 0 22px 0; border: 1px solid #FFFFFF; padding: 12px 17px; background-color: #FAF8F8; }
 legend { font-size: 1.1em; background-color: #095D92; color: #FFFFFF; font-weight: bold; padding: 4px 8px; }
 
-label.float { float: left; display: block; width: 100px; margin: 4px 0 0 0; clear: left; }
-label { display: block; width: auto; margin: 0 0 10px 0; }
+label.float { float: left; display: block; width: 250px; margin: 4px 0 0 0; clear: left;}
+label.gender { float: left; display: block; width: 440px; margin: 4px 0 0 0; clear: left;}
+label { display: block; width: auto; margin: 0 0 10px 0; margin-bottom: 12px;}
 label.spam-protection { display: inline; width: auto; margin: 0; }
 
-input.inp-text, textarea, input.choose, input.answer { border: 1px solid #909090; padding: 4px; }
-input.inp-text { width: 300px; margin: 0 0 8px 0; }
+input.inp-text, textarea, input.choose, input.answer { border: 1px solid #909090; padding: 6px; padding-top: 12px;}
+input.inp-text { width: 450px; margin: 0 0 8px 0;  margin-bottom: 15px;}
 textarea { width: 400px; height: 150px; margin: 0 0 12px 0; display: block; }
+
+.gender{padding-top: 12px; padding-bottom: 12px; margin-bottom: 15px;}
+
+.datafile{color: #FFFFFF;}
 
 input.choose { margin: 0 2px 0 0; }
 input.answer { width: 40px; margin: 0 0 0 10px; }
@@ -54,24 +59,27 @@ form br { display: none; }
 				<input class="inp-text" name="input-first-name" id="input-first-name" type="text" size="30" disabled /><br />
 
 				<label for="middle-name" class="float"><strong>Middle Name:</strong></label><br />
-				<input class="inp-text" name="input-Middle-name"  id="input-middle-name" type="text" size="30" disabled />
+				<input class="inp-text" name="input-Middle-name"  id="input-middle-name" type="text" size="30"disabled  />
 				
 				<label for="last-name" class="float"><strong>Last Name:</strong></label><br />
-				<input class="inp-text" name="last-name"  id="last-name" type="text" size="30" disabled />
+				<input class="inp-text" name="last-name"  id="last-name" type="text" size="30"disabled  />
+				<br>
+				<div>
+				<label for="Gender" class="gender"><strong>Primarly Identified as: &nbsp;</strong>  
+				<input class="gender" type="radio" name="gender" value="male" / > Male 
+				<input class="gender" type="radio" name="gender" value="female" /> Female 
+				<input class="gender" type="radio" name="gender" value="other" /> Prefer Not To Disclose
+				</label> 
+				</div>
 				
-				<label for="Gender" class="float"><strong>Primarly Identified as:</strong></label><br />
-				<input type="radio" name="gender" value="male" /> Male
-				<input type="radio" name="gender" value="female" /> Female
-				<input type="radio" name="gender" value="other" /> Prefer Not To Disclose
 				
-				
-				<label for="Gender" class="float"><strong>Date of Birth:</strong></label><br />
-				<input class="inp-text" name="dob" id="dob" type="date" size="30" value="" min="1950-01-01" max="2018-12-31"  disabled/>
+				<label for="dob" class="float"><strong>Date of Birth:</strong></label><br />
+				<input class="inp-text" name="dob" id="dob" type="date" size="30" value="" min="1950-01-01" max="2018-12-31" disabled />
 				<label for="e-mail" class="float"><strong>E-Mail:</strong></label><br />
 				<input class="inp-text" name="e-mail"  id="e-mail" type="text" size="30" disabled />
 				
 				<label for="mobile-no" class="float"><strong>Mobile No:</strong></label><br />
-				<input class="inp-text" name="mobile-no"  id="mobile-no" type="text" size="30" disabled />
+				<input class="inp-text" name="mobile-no"  id="mobile-no" type="text" size="30" disabled  />
 		</fieldset> 
 		<!-- ============================== Fieldset 2 end ============================== -->
 
@@ -80,21 +88,22 @@ form br { display: none; }
 		<fieldset>
 		<legend>Address:</legend>
 			<label for="adrdess1" class="float"><strong>Address Line 1</strong></label><br />
-				<input class="inp-text" name="input-adrdess1" id="input-adrdess1" type="text" size="30" disabled /><br />
+				<input class="inp-text" name="input-adrdess1" id="input-adrdess1" type="text" size="30" disabled/><br />
 
 				<label for="adrdess2" class="float"><strong>Address Line 2</strong></label><br />
-				<input class="inp-text" name="input-adrdess2"  id="input-adrdess2" type="text" size="30" disabled />
+				<input class="inp-text" name="input-adrdess2"  id="input-adrdess2" type="text" size="30"disabled  />
 				
 				<!--<label for="adrdess3" class="float"><strong>Address Line 3</strong></label><br />
 				<input class="inp-text" name="input-adrdess3"  id="input-adrdess3" type="text" size="30" disabled /> -->
 				
 				<label for="city" class="float"><strong>City:</strong></label><br />
 				<input class="inp-text" name="input-city"  id="input-city" type="text" size="30" disabled />
+				
 				<label for="Zip-code" class="float"><strong>Zip Code:</strong></label><br />
-				<input class="inp-text" name="input-code"  id="input-code" type="text" size="30" disabled />
+				<input class="inp-text" name="input-code"  id="input-code" type="text" size="30"disabled />
 				
 				<label for="county" class="float"><strong>County:</strong></label><br />
-				<input class="inp-text" name="county"  id="county" type="text" size="30" disabled />
+				<input class="inp-text" name="county"  id="county" type="text" size="30"disabled />
 				
 				<label for="country" class="float"><strong>Country:</strong></label><br />
 				<input class="inp-text" name="country"  id="country" type="text" size="30" disabled />
@@ -105,8 +114,7 @@ form br { display: none; }
 		<fieldset>
 		<legend>Resume Upload:</legend>
 				<label for="file-upload" class="float"><strong>Upload Resume:</strong></label><br />
-				<input type="file" name="datafile" id="datafile" size="30" disabled />
-				*can accept only pdf and .docs file
+				<input class= "datafile" type="file" name="datafile" id="datafile" size="55" accept= ".pdf, .doc, .docx" /> *can accept only pdf or .docs file
 				
 		</fieldset>
 		
@@ -114,7 +122,7 @@ form br { display: none; }
 		<!--<p><input class="upload-button" type="upload-button" alt="UPLOAD" id="upload-button" name="UPLOAD" value="Upload" disabled /></p>-->
 		
 		<input class="edit-button" type="button" alt="edit-button" id="edit-button" name="edit-button" value="Edit" onclick="kk1()"/>
-		<p><input class="submit-button" type="submit" alt="SUBMIT" id="submit" name="Submit" value="Update" disabled /></p>
+		<p><input class="submit-button" type="submit" alt="SUBMIT" id="submit" name="Submit" value="Update"  /></p>
 		
 
 		
@@ -140,6 +148,7 @@ form br { display: none; }
 		document.getElementById("country").disabled  = false;
 		document.getElementById("e-mail").disabled  = false;
 		document.getElementById("mobile-no").disabled  = false;
+		document.getElementById("input-code").disabled  = false;
 		//document.getElementById("upload-button").disabled=false
 		document.getElementById("submit").disabled  = false;
 		document.getElementById("datafile").disabled  = false;
@@ -147,10 +156,8 @@ form br { display: none; }
 		
 		
     }
-    
+
 </script>
-
-
 
 
 
