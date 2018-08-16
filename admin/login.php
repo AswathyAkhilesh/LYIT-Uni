8be139ad19ec73424beeefdf026c90eb4d91e825
ConorDorrian1 <L00101441@student.lyit.ie>
2018-08-16 19:48:37 +0100

    Simple Admin login page

---
+++ admin/login.php
@@ -0,0 +1,29 @@
+<!-- Header -->
+<?php include ('header.php'); ?>
+
+<!-- Your Content Start-->
+<!-- Admin Login -->
+<section>
+<form class = "comment-form container">
+<br>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="f rm-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check">
+  </div>
+  <div class="site-btn text-white">
+     <a href="index.php">Login</a>
+     </div>
+ <br> 
+</form>
+</section>
+<!-- Your Content End-->
+
+<!-- Footer -->
+<?php include ('footer.php'); ?>
+<!-- Header End - Do Not Remove -->
