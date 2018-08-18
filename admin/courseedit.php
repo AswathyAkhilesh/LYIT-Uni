485101d7c11d8c466ea216f529654ad0b14bc20e
unknown <L00149517@student.lyit.ie>
2018-08-18 22:11:54 +0100

    Admin Course Edit html page modified for php rendering.

--- admin/courseedit.php
+++ admin/courseedit.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Course Edit</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
   <script src="http://js.nicedit.com/nicEdit-latest.js" type="text/javascript"></script>
   <script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>
-</head>
-<body>
+
 <div class="container">
   <h2>Edit Course Details</h2>
   <p>The form below contains course details, modify as required and click on the "Update" button to save the details.</p>
@@ -65,10 +59,7 @@
             <li>Cloud DevOps Engineer</li>
             <li>Systems Software Engineer</li>
          </ul>
-	  
-	  
-	  
-	  
+
 	  </textarea>
     </div>
 	<div>
@@ -98,5 +89,3 @@
   </form>
 </div>
 
-</body>
-</html>
