b30f4cb29b3b5c1cc9d691af8c4cc58152e348a0
L00130494 <L00130494@student.lyit.ie>
2018-08-20 19:57:12 +0100

    added interview list page

---
+++ admin/interviewlist.php
@@ -0,0 +1,74 @@
+<?php include ('header.php'); ?>
+<!--Style sheet--->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/select/1.2.7/css/select.dataTables.min.css">
+<link rel="stylesheet" href="../css/admincourselist.css">
+<!--Third-party Libraries--->
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://cdn.datatables.net/select/1.2.7/js/dataTables.select.min.js"></script>
+<!--Script-->
+<script src="../js/admincourselist.js"></script>
+<!--Begin HTML design--> 
+<br>
+<h2>Interview List</h2>
+<p>The below grid contains the applicant list, to view an applicant select an applicant and click on the "view" button</p>
+<table id="example" class="table table-striped table-bordered" style="width:100%">
+   <thead>
+      <tr>
+         <th></th>
+         <th>Name</th>
+         <th>Course Name</th>
+         <th>Date</th>
+         <th>Time</th>
+      </tr>
+   </thead>
+   <tbody>
+      <tr>
+         <td></td>
+         <td>Aswathy</td>
+         <td>Devops</td>
+         <td>21 Aug</td>
+         <td>9.00 AM</td>
+      </tr>
+      <tr>
+         <td></td>
+         <td>Roshni</td>
+         <td>Devops</td>
+         <td>21 Aug</td>
+         <td>9.30 AM</td>
+      </tr>
+      <tr>
+         <td></td>
+         <td>Matthew</td>
+         <td>Devops</td>
+         <td>21 Aug</td>
+         <td>10.00 AM</td>
+      </tr>
+      <tr>
+         <td></td>
+         <td>Conor</td>
+         <td>Devops</td>
+         <td>21 Aug</td>
+         <td>10.30 AM</td>
+      </tr>
+      <tr>
+         <td></td>
+         <td>Liam</td>
+         <td>Devops</td>
+         <td>21 Aug</td>
+         <td>11.00 AM</td>
+      </tr>
+      <tr>
+         <td></td>
+         <td>Ruth</td>
+         <td>Devops</td>
+         <td>21 Aug</td>
+         <td>11.30 AM</td>
+      </tr>
+   </tbody>
+</table>
+<p align="center">
+   <button type="button" onclick="location.href='\interview.php';" class="btn btn-info">View</button>
+</p>
+<?php include ('footer.php'); ?>
