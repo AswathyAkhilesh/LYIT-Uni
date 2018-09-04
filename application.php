54a93bf9824f046607b35928c0ae7e85547898aa
MatthewBergin <l00095247@student.lyit.ie>
2018-09-04 20:31:18 +0100

    Added course title header and changed format of table

--- application.php
+++ application.php
@@ -1,56 +1,57 @@
 <?php include ('header.php'); ?>
 <section class="container spad">
         <div class="container" style="width: 30%">
-                <table class="table table-striped">
+        <h2 style = "padding-bottom: 20px">Course Title Here</h2>
+                <table>
                    <tbody>
                       <tr>
                          <td colspan="1">
                             <form class="well form-horizontal">
                                <fieldset>
                                   <div class="form-group">
-                                     <label class="col-md-4 control-label">First Name</label>
+                                     <label class="col-md-6 control-label">First Name</label>
                                      <div class="col-md-8 inputGroupContainer" >
                                         <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span><input id="firstName" name="firstName" placeholder="First Name" class="form-control" required="true" value="" type="text"></div>
                                      </div>
                                   </div>
                                   <div class="form-group">
-                                     <label class="col-md-4 control-label">Last Name</label>
+                                     <label class="col-md-6 control-label">Last Name</label>
                                      <div class="col-md-8 inputGroupContainer">
                                         <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span><input id="lastName" name="lastName" placeholder="Last Name" class="form-control" required="true" value="" type="text"></div>
                                      </div>
                                   </div>
                                   <div class="form-group">
-                                     <label class="col-md-4 control-label">Address Line 1</label>
+                                     <label class="col-md-6 control-label">Address Line 1</label>
                                      <div class="col-md-8 inputGroupContainer">
                                         <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="addressLine1" name="addressLine1" placeholder="Address Line 1" class="form-control" required="true" value="" type="text"></div>
                                      </div>
                                   </div>
                                   <div class="form-group">
-                                     <label class="col-md-4 control-label">Address Line 2</label>
+                                     <label class="col-md-6 control-label">Address Line 2</label>
                                      <div class="col-md-8 inputGroupContainer">
                                         <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="addressLine2" name="addressLine2" placeholder="Address Line 2" class="form-control" required="true" value="" type="text"></div>
                                      </div>
                                   </div>
                                   <div class="form-group">
-                                     <label class="col-md-4 control-label">City</label>
+                                     <label class="col-md-6 control-label">City</label>
                                      <div class="col-md-8 inputGroupContainer">
                                         <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="city" name="city" placeholder="City" class="form-control" required="true" value="" type="text"></div>
                                      </div>
                                   </div>
                                   <div class="form-group">
-                                     <label class="col-md-4 control-label">County</label>
+                                     <label class="col-md-6 control-label">County</label>
                                      <div class="col-md-8 inputGroupContainer">
                                         <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="county" name="county" placeholder="County" class="form-control" required="true" value="" type="text"></div>
                                      </div>
                                   </div>
                                   <div class="form-group">
-                                     <label class="col-md-4 control-label">EirCode (Optional)</label>
+                                     <label class="col-md-6 control-label">EirCode (Optional)</label>
                                      <div class="col-md-8 inputGroupContainer">
                                         <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="eircode" name="eircode" placeholder="EirCode" class="form-control" value="" type="text"></div>
                                      </div>
                                   </div>
                                   <div class="form-group">
-                                     <label class="col-md-4 control-label">Country</label>
+                                     <label class="col-md-6 control-label">Country</label>
                                      <div class="col-md-8 inputGroupContainer">
                                         <div class="input-group">
                                            <span class="input-group-addon" style="max-width: 100%;"><i class="glyphicon glyphicon-list"></i></span>
@@ -309,13 +310,13 @@
                                      </div>
                                   </div>
                                   <div class="form-group">
-                                     <label class="col-md-4 control-label">Email</label>
+                                     <label class="col-md-6 control-label">Email</label>
                                      <div class="col-md-8 inputGroupContainer">
                                         <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span><input id="email" name="email" placeholder="Email" class="form-control" required="true" value="" type="text"></div>
                                      </div>
                                   </div>
                                   <div class="form-group">
-                                     <label class="col-md-4 control-label">Phone Number</label>
+                                     <label class="col-md-6 control-label">Phone Number</label>
                                      <div class="col-md-8 inputGroupContainer">
                                         <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span><input id="phoneNumber" name="phoneNumber" placeholder="Phone Number" class="form-control" required="true" value="" type="text"></div>
                                      </div>
