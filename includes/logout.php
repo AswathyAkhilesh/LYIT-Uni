102eb6253c1b1efe2eac773c79158598e06e97a2
L00113360 <33516120+L00113360@users.noreply.github.com>
2018-09-13 22:39:35 +0100

    Added _SESSION = null to the user session on logout.

--- includes/logout.php
+++ includes/logout.php
@@ -7,6 +7,14 @@ $_SESSION['db_username'] = null;
 $_SESSION['db_firstName'] = null;
 $_SESSION['db_lastName'] = null;
 $_SESSION['db_userRole'] = null;
+$_SESSION['db_email'] = null;
+$_SESSION['db_dob'] = null;
+$_SESSION['db_phoneNumber'] = null;
+$_SESSION['db_address'] = null;
+$_SESSION['db_city'] = null;
+$_SESSION['db_county'] = null;
+$_SESSION['db_eirCode'] = null;
+$_SESSION['db_country'] = null;
 
 header("Location: ../index.php");
 
