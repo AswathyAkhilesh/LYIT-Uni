1d7972bfd2333f481d466db89b573ffb7317357f
MatthewBergin <l00095247@student.lyit.ie>
2018-09-13 20:21:02 +0100

    Added about us text for LYIT

--- about.php
+++ about.php
@@ -8,20 +8,28 @@
 <section class="about-section spad">
 		<div class="container">
 			<div class="section-title text-center">
-				<h3>WELCOME TO EZUCA</h3>
-				<p>Let children creative and make a different</p>
+				<h3>WELCOME TO LETTERKENNY INSTITUTE OF TECHNOLOGY</h3>
+				<p>WHERE STUDENTS COME TO LEARN</p>
 			</div>
 			<div class="row">
 				<div class="col-lg-6 about-text">
 					<h5>About us</h5>
-					<p>Lorem ipsum dolor sitdoni amet, consectetur dont adipis elite. Vivamus interdum ultrices augue. Aenean dos cursus lania. Duis et fringilla leonardo. Mauris mattis phare sem, debut curus risus viverra sed.</p>
+					<p style = "text-align: justify">Letterkenny Institute of Technology (LYIT) fosters one of the most progressive learning environments in Ireland. <br><br>
+					 Our reach is both local and international; attracting a diverse student body of more than 4,000 students from Ireland and 31 countries across the globe.
+					 They choose LYIT because of our unique ethos that harnesses academic excellence with career-focused practical experience.
+					 It’s an approach that positions our students for future success in line with their aspirations.
+
+We also foster close relationships with the wider local community. Our engagement and partnerships with indigenous and international businesses leaders strengthens our student’s prospects in tandem with the prosperity of the region’s economy.
+
+Our modern integrated campuses in Letterkenny and Killybegs aren’t just gateways to a bright future, but to one of the most breathtaking corners of the world - with Donegal named National Geographic’s ‘Coolest Place on the Planet 2017’.
+
+There’s just so much to discover. So take your time, and stay a while to explore our website and see why LYIT is the place to be.</p>
 					<h5 class="pt-4">Our history</h5>
-					<p>Led at felis arcu. Integer lorem lorem, tincidunt eu congue et, mattis ut ante. Nami suscipit, lectus id efficitur ornare, leo libero convalis nulla, vitae dignissim .</p>
 					<ul class="about-list">
-						<li><i class="fa fa-check-square-o"></i> University Faculties organise teaching and research into individual subjects.</li>
-						<li><i class="fa fa-check-square-o"></i> The University is rich in history - its famous buildings attract visitors.</li>
-						<li><i class="fa fa-check-square-o"></i> 50 years of people, and achievements that continue to transform.</li>
-						<li><i class="fa fa-check-square-o"></i> The University's core values are as follows:freedom of thought.</li>
+						<li><i class="fa fa-check-square-o"></i> The institute is one of the original networks of Regional Technical Colleges established in Ireland.</li>
+						<li><i class="fa fa-check-square-o"></i> LYIT opened in 1971, with Danny O'Hare as first principal.</li>
+						<li><i class="fa fa-check-square-o"></i> The institute was also referred to as Donogh O'Malley Regional Technical College, Letterkenny, until 1992, in honor of Donogh O'Malley, T.D., the reforming Minister for Education.</li>
+						<li><i class="fa fa-check-square-o"></i> In 2007, LYIT came joint second with Tallaght IT to the Galway-Mayo IT in The Sunday Times University Guide 2007, which ranks the top universities and IT's.</li>
 					</ul>
 				</div>
 				<div class="col-lg-6 pt-5 pt-lg-0">
