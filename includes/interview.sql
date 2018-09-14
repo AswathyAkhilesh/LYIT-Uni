e6f83a1c57322b18d6d8c99cdda8f05510c21bb1
unknown <L00149517@student.lyit.ie>
2018-09-14 22:23:10 +0100

    Updated interview.sql with courses schema and initial seed data

--- includes/interview.sql
+++ includes/interview.sql
@@ -47,30 +47,31 @@ INSERT INTO `admin` (`adminID`, `adminName`, `adminEmail`, `adminPass`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `courselist`
+-- Table structure for table `courses`
 --
-
-CREATE TABLE `courselist` (
-  `courseID` int(11) NOT NULL,
-  `courseName` varchar(255) NOT NULL,
-  `courseDesc` text NOT NULL,
-  `entryRequirements` varchar(255) NOT NULL,
-  `courseFees` varchar(255) NOT NULL,
-  `jobs` varchar(255) NOT NULL,
-  `award` varchar(255) NOT NULL,
-  `credit` varchar(255) NOT NULL,
-  `duration` varchar(255) NOT NULL,
-  `studyMode` varchar(255) NOT NULL,
-  `appDeadline` varchar(255) NOT NULL
+DROP TABLE IF EXISTS `courses`;
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL,
+  `coursecode` varchar(10) NOT NULL,
+  `coursename` varchar(50) NOT NULL,
+  `bannerdescription` varchar(500) NOT NULL,
+  `coursedetails` varchar(1000) NOT NULL,
+  `entryrequirements` varchar(700) NOT NULL,
+  `coursefees` varchar(500) CHARACTER SET utf8 NOT NULL,
+  `jobs` varchar(800) NOT NULL,
+  `duration` varchar(20) NOT NULL,
+  `studymode` varchar(50) NOT NULL,
+  `award` varchar(10) NOT NULL,
+  `credit` int(11) NOT NULL,
+  `applicationdeadline` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `courselist`
+-- Dumping data for table `courses`
 --
 
-INSERT INTO `courselist` (`courseID`, `courseName`, `courseDesc`, `entryRequirements`, `courseFees`, `jobs`, `award`, `credit`, `duration`, `studyMode`, `appDeadline`) VALUES
-(1, 'Applied Computing', 'Our Master of Science in Computing in DevOps is a one year, full-time or two year, part-time programme. DevOps is the practice of operations and development engineers participating together in the entire service lifestyle, from design through the development process to production support. DevOps is characterised by operations staff harnessing the same techniques as developers for their systems work. Topics the course covers include: DevOps Software Engineering, Project Management and Deployment Pipelines.\r\n\r\nProposed Module for this year are as Follows:- \r\n\r\nDevOps Software Engineering -	Mandatory\r\n\r\nOO Programming for Server Administration-Mandatory\r\n\r\nScripting the Deployment Pipeline-Mandatory\r\n\r\nDevOps Project Management-Mandatory\r\n\r\nDissertation-Mandatory', '', '', '', '', '', '', '', ''),
-(2, 'DevOps', 'DevOps, a new organizational and cultural way of organizing development and IT operations work, is spreading rapidly – driven by mounting evidence of its benefits to the business. But reaping these gains requires rethinking application security to deliver more secure code at DevOps speed.', '', '', '', '', '', '', '', '');
+INSERT INTO `courses` (`id`, `coursecode`, `coursename`, `bannerdescription`, `coursedetails`, `entryrequirements`, `coursefees`, `jobs`, `duration`, `studymode`, `award`, `credit`, `applicationdeadline`) VALUES
+(1, 'COOD1', 'Master of Science in DevOps', 'DevOps, a new organizational and cultural way of organizing development and IT operations work, is spreading rapidly, driven by mounting evidence of its benefits to the business. But reaping these gains requires rethinking application security to deliver more secure code at DevOps speed.', '<br><br><b>Our Master of Science in Computing in DevOps is a one year, full-time or two year, part-time programme. DevOps is the practice of operations and development engineers participating together in the entire service lifestyle, from design through the development process to production support.\r\n         DevOps is characterised by operations staff harnessing the same techniques as developers for their systems work. Topics the course covers include: DevOps Software Engineering, Project Management and Deployment Pipelines.</b><br><br> Proposed Module for this year are as Follows:-  <br><br> DevOps Software Engineering -	Mandatory<br><br>OO Programming for Server Administration-Mandatory<br><br>Scripting the Deployment Pipeline-Mandatory<br><br>DevOps Project Management-Mandatory<br><br>Dissertation-Mandatory', '<b><br>Minimum Entry Requirements</b><br><br>\r\n         - Level 8 Honours Degree in Computing, or equivalent, second class honours (2.2), or Higher Diploma in Computing (Conversion Course into Computing).<br> \r\n         -If you do not have an honours degree but have relevant experience you may also be eligible to apply via Recognition of Prior Learning (RPL).  \r\n         <br><br>\r\n         <table class=\"cao-table\">\r\n            <tbody>\r\n               <tr>\r\n                  <th>Number of Places</th>\r\n                  <th>15</th>\r\n               </tr>\r\n            </tbody>\r\n         </table>', '<br><div style=\"padding-bottom:15px\"><b>The EU and NON EU citizens have got different fee structure</b></div>\r\n         <div style=\"color:#22206f;font-size:15px;padding-bottom:10px\">EU Fees: &euro;5,000.00</div>\r\n         <div style=\"color:#22206f;font-size:15px\">Non-EU Fees: &euro;9,000.00</div><br>', '<br>\r\n         <div>Career Pathways</div>\r\n         <p>The main employers are:</p>\r\n         <ul style=\"margin-left:30px;font-size:14px\">\r\n            <li>Software Companies</li>\r\n            <li>Multinational Companies</li>\r\n         </ul>\r\n		 <br>\r\n         <div>Graduate Careers</div>\r\n         <p>Former graduates are employed in the following capacities:</p>\r\n         <ul style=\"margin-left:30px;font-size:14px\">\r\n            <li>DevOps Engineer</li>\r\n            <li>Cloud DevOps Engineer</li>\r\n            <li>Systems Software Engineer</li>\r\n         </ul>', '1 and 2 year', 'Full-time and Part-time', 'Masters', 30, '24 august');
 
 -- --------------------------------------------------------
 
