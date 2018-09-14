f35610bdbc1e185ff9be4a74c45c185b9810c2c4
AswathyAkhilesh <L00149517@student.lyit.ie>
2018-09-14 22:36:58 +0100

    Added code to retrive data from courses table with default hard coded value

---
+++ includes/course.php
@@ -0,0 +1,23 @@
+<?php
+
+    $query = "SELECT * FROM courses WHERE id = 1 ";// Get course details
+    $select_course_query = mysqli_query($connection ,$query);
+    if(!$select_course_query) {
+        die("QUERY FAILED". mysqli_error($connection));
+    }
+
+    while($row = mysqli_fetch_array($select_course_query)) {
+        $db_coursecode = $row['coursecode']; // Display Result
+		$db_coursename = $row['coursename']; // Display Result
+		$db_bannerdescription = $row['bannerdescription']; // Display Result
+		$db_coursedetails = $row['coursedetails']; // Display Result
+		$db_entryrequirements = $row['entryrequirements']; // Display Result
+		$db_coursefees = $row['coursefees']; // Display Result		
+		$db_jobs = $row['jobs']; // Display Result	
+		$db_duration = $row['duration']; // Display Result		
+		$db_studymode = $row['studymode']; // Display Result	
+		$db_award = $row['award']; // Display Result	
+		$db_credit = $row['credit']; // Display Result	
+		$db_applicationdeadline = $row['applicationdeadline']; // Display Result			
+    }
+?>
