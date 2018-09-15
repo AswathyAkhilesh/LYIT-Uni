168cefb378714efdc64d76aced32667ae81f9c8b
AswathyAkhilesh <L00149517@student.lyit.ie>
2018-09-15 04:39:24 +0100

    Assigned posted form data to variables.

---
+++ includes/courseadd.php
@@ -0,0 +1,17 @@
+<?php
+if(isset($_POST['save'])) {
+
+    $coursecode = $_POST['textbox1'];
+    $coursename= $_POST['textbox2'];
+	$coursedetails= $_POST['comment1'];
+	$entryrequirements= $_POST['comment2'];
+	$fee= $_POST['comment3'];
+	$job= $_POST['comment4'];
+	$banner= $_POST['comment5'];
+	$award= $_POST['textbox3'];
+	$credit= $_POST['textbox4'];
+	$duration= $_POST['textbox5'];
+	$studymode= $_POST['textbox6'];
+	$applicationdeadline= $_POST['textbox7'];
+}
+?>
