9bcb7edcb476b7c9696913bb74e58e1cb3bb9ca2
AswathyAkhilesh <L00149517@student.lyit.ie>
2018-09-15 05:08:10 +0100

    Updated js file to point to new sever side php data source.

--- js/admincourselist.js
+++ js/admincourselist.js
@@ -1,15 +1,27 @@
 var table;
 $(document).ready(function() {
     table = $('#example').DataTable( {
-			columnDefs: [ {
-				orderable: false,
-				className: 'select-checkbox',
-				targets:   0
-			} ],
-			select: {
-				style:    'os',
-				selector: 'td:first-child'
-			},
-			order: [[ 1, 'asc' ]]
-		} );
+		"processing": true,
+        "serverSide": true,
+        "ajax": "../includes/datatablecourselist.php",
+        "columns": [
+            { "data": null, "defaultContent": "" },
+            { "data": "coursecode" },
+            { "data": "coursename" },
+            { "data": "studymode" },
+            { "data": "award" },
+            { "data": "credit" },
+            { "data": "applicationdeadline" }
+        ],
+        columnDefs: [ {
+            orderable: false,
+            className: 'select-checkbox',
+            targets:   0
+        } ],
+        select: {
+            style:    'os',
+            selector: 'td:first-child'
+        },
+        order: [[ 1, 'asc' ]]
+    } );
 } );
