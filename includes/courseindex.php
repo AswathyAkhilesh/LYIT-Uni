30fadcc57259dc888af98f2e2ed71cc9d814d874
AswathyAkhilesh <L00149517@student.lyit.ie>
2018-09-15 03:46:31 +0100

    Updated courseindex.php with code to retreive applied computing id

--- includes/courseindex.php
+++ includes/courseindex.php
@@ -1,5 +1,5 @@
 <?php
-    $querydevops = "SELECT * FROM courses WHERE coursecode = 'COOD1' ";// Check if the user exists
+    $querydevops = "SELECT * FROM courses WHERE coursecode = 'COOD1' ";
     $select_courseid_query = mysqli_query($connection ,$querydevops);
     if(!$select_courseid_query) {
         die("QUERY FAILED". mysqli_error($connection));
@@ -7,5 +7,15 @@
 
     while($row = mysqli_fetch_array($select_courseid_query)) {
         $db_devops = $row['id']; // Display Result			
+    }
+	
+	$queryappliedcomputing = "SELECT * FROM courses WHERE coursecode = 'COOD2' ";
+    $select_courseid_query = mysqli_query($connection ,$queryappliedcomputing);
+    if(!$select_courseid_query) {
+        die("QUERY FAILED". mysqli_error($connection));
+    }
+
+    while($row = mysqli_fetch_array($select_courseid_query)) {
+        $db_appliedcomputing = $row['id']; // Display Result			
     }
 ?>
