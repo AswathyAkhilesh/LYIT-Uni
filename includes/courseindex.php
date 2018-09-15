ee60339500b3f1330cf38f03380d9c352b490143
AswathyAkhilesh <L00149517@student.lyit.ie>
2018-09-15 03:58:06 +0100

    Updated courseindex.php with code to retreive cloud technology id

--- includes/courseindex.php
+++ includes/courseindex.php
@@ -20,4 +20,14 @@
     while($row = mysqli_fetch_array($select_courseid_query)) {
         $db_appliedcomputing = $row['id']; // Display Result			
     }
+	
+	$querycloudtechnology = "SELECT * FROM courses WHERE coursecode = 'COOD3' ";// Check if the user exists
+    $select_courseid_query = mysqli_query($connection ,$querycloudtechnology);
+    if(!$select_courseid_query) {
+        die("QUERY FAILED". mysqli_error($connection));
+    }
+
+    while($row = mysqli_fetch_array($select_courseid_query)) {
+        $db_cloudtechnology = $row['id']; // Display Result			
+    }
 ?>
