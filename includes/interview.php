fba2af196477df673edc08ab759c9fdc55d96d17
L00130494 <L00130494@student.lyit.ie>
2018-09-16 23:25:12 +0100

    added interview file for populating data for interview admin page

---
+++ includes/interview.php
@@ -0,0 +1,28 @@
+<?php
+
+
+if(isset($_GET['id'])) {
+
+    $userID = $_GET['id'];
+
+    $query = "SELECT * FROM users WHERE userID = {$userID}"; // Get user details
+    $select_user_query = mysqli_query($connection ,$query);
+    if(!$select_user_query) {
+        die("QUERY FAILED". mysqli_error($connection));
+    }
+
+    while($row = mysqli_fetch_array($select_user_query)) {
+        $db_firstName = $row['firstName']; // Display Result
+        $db_lastName = $row['lastName']; // Display Result
+        $db_email = $row['email']; // Display Result
+        $db_dob = $row['dob']; // Display Result
+		$db_gender = $row['gender']; // Display Result
+        $db_phoneNumber = $row['phoneNumber']; // Display Result
+        $db_address = $row['address']; // Display Result
+        $db_city = $row['city']; // Display Result
+        $db_county = $row['county']; // Display Result
+        $db_eirCode = $row['eirCode']; // Display Result
+        $db_country = $row['country']; // Display Result
+        $db_userRole= $row['userRole']; // Display Result
+    }  
+}
