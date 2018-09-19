7a6e691a8938b215c01fbe03b42d2d0d789693ee
MatthewBergin <l00095247@student.lyit.ie>
2018-09-19 02:34:01 +0100

    Add applicant to database

--- includes/applicantadd.php
+++ includes/applicantadd.php
@@ -1,22 +1,27 @@
 <?php include 'db.php'; ?>
+<?php include 'course.php'; ?>
 <?php
+
+
 if(isset($_POST['submit'])) {
 
+
+    $courseID = $_POST['hiddenid'];
+    $coursename = $_POST['coursename'];
+    $firstname = $_POST['input-first-name'];
     $userID = $_POST['userID'];
-    $courseID = $_POST['courseID'];
-    $FirstName = $_POST['FirstName'];
-    $CourseName = $_POST['CourseName'];
+
     
 
     $query = "INSERT INTO `applicants` ( `userID`, `courseID`, `FirstName`, `CourseName`) VALUES
- ('{$userID}', '{$courseID}', '{$FirstName}', '{$CourseName}')";
+ ('{$userID}', '{$courseID}', '{$firstname}', '{$coursename}')";
     $select_user_query = mysqli_query($connection ,$query);
     if(!$select_user_query) {
         die("QUERY FAILED". mysqli_error($connection));
     }
 	else
 	{
-		header("Location: ../application.php");
+		header("Location: ../application.php?id=$courseID");
 	}
 }
 ?>
