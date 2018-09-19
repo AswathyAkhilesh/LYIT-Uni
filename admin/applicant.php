ec7fb6c6b38ab9297921e229143f44383572d537
AswathyAkhilesh <L00149517@student.lyit.ie>
2018-09-19 10:42:50 +0100

    upadted admin applicant.php file

--- admin/applicant.php
+++ admin/applicant.php
@@ -1,8 +1,5 @@
 <?php include ('header.php'); ?>
 <?php include ('../includes/applicant.php'); ?>
-<?php include ('../includes/course.php'); ?>
-<?php include ('../includes/profile.php'); ?>
-
 
 <section Class="container spad">
 
@@ -16,69 +13,99 @@
             <div class="row">
             <div class="col-md-1 col-md-1 ">  </div>
                 
-            <form action="../includes/scheduleinterview.php" method="post"> 
-
-            <input type="hidden" name="hiddenid" id="hiddenid" value="<?php echo $_GET['id'] ?>" />
-            <input type="hidden" name="coursename" id="coursename" value="<?php echo $db_coursename ?>" />
-            <input type="hidden" name="coursecode" id="coursecode" value="<?php echo $db_coursecode ?>" />
-
+                
             <div class=" col-md-9 col-lg-12 " style = "padding-top: 30px"> 
             <table class="table table-user-information">
                 <thead>
                     <h4 style = "padding-left: 10px">Personal Details<h4>
                 </thead>
                 <tbody>
+				
                     <tr>
                         <td>First Name:</td>
-                        <td><input class="inp-text" name="input-first-name" id="input-first-name" type="text" value="<?php echo $db_firstName ?>" size="30" disabled/></td>
+                        <td><?php echo $db_firstName ?></td>
                     </tr>
                     <tr>
-                        <td>Last Name:</td>
-                        <td><input class="inp-text" name="last-name"  id="last-name" type="text" value="<?php echo $db_lastName ?>" size="30" disabled/></td>
+                        <td>Surname:</td>
+                        <td><?php echo $db_lastName ?></td>
                     </tr>
                     <tr>
                         <td>Date of Birth:</td>
-                        <td><input class="inp-text" name="dob" id="dob" type="date" value="<?php echo date('Y-m-d', strtotime($db_dob)) ?>" size="30" min="1950-01-01" max="2018-12-31" disabled/></td>
+                        <td><?php echo date('Y-m-d', strtotime($db_dob)) ?></td>
                     </tr>
                     <tr>
                         <td>Identify as:</td>
-                        <td><input class="inp-text" name="gender"  id="gender" type="text" value="<?php echo $db_gender ?>" size="30" disabled/></td>
+                        <td><?php echo $db_gender ?></td>
                     </tr>
                     <tr>
                         <td>Address 1:</td>
-                        <td><input class="inp-text" name="input-address1" id="input-address1" type="text" size="30" value="<?php echo $db_address ?>" disabled /></td>
+                        <td><?php echo $db_address ?></td>
                     </tr>
                     <tr>
                         <td>County:</td>
-                        <td><input class="inp-text" name="county"  id="county" type="text" size="30" value="<?php echo $db_county ?>" disabled/></td>
+                        <td><?php echo $db_county ?></td>
                     </tr>
                     <tr>
                         <td>Country:</td>
-                        <td><input class="inp-text" name="country"  id="country" type="text" size="30" value="<?php echo $db_country ?>" disabled/></td>
+                        <td><?php echo $db_country ?></td>
                     </tr>
                     <tr>
                         <td>Email:</td>
-                        <td><input class="inp-text" name="e-mail"  id="e-mail" type="text" size="30" value="<?php echo $db_email ?>" disabled/></td>
+                        <td><a href="mailto:email@email.com"><?php echo $db_email ?></a></td>
                     </tr>
                         <td>Phone Number:</td>
-                        <td><input class="inp-text" name="mobile-no" id="mobile-no" type="number" max="0999999999" value="<?php echo $db_phoneNumber ?>" disabled/></td>     
+                        <td><?php echo $db_phoneNumber ?></td>     
                     </tr>
                      
                     </tbody>
                     </table>
 
                 
-                
+                <table class="table table-user-information">
+                <thead>
+                    <h4 style = "padding-left: 10px">Application Details<h4>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Applied Course:</td>
+                        <td><?php echo $db_CourseName?></td>
+                    </tr>
+                    <tr>
+                        <td>Qualifications:</td>
+                        <td><?php echo $db_highestQual?></td>
+                    </tr>
+                    <tr>
+                        <td>Work Experience:</td>
+                        <td><?php echo $db_workExp?></td>
+                    </tr>
+					<form action="../includes/applicant.php" method="post">
+					<input type="hidden" name="userid" id="hiddenid" value="<?php echo $_GET['id'] ?>" />
+					<input type="hidden" name="courseid" id="hiddenid" value="<?php echo $db_CourseID ?>" />
+					<input type="hidden" name="coursename" id="hiddenid" value="<?php echo $db_CourseName ?>" />
+					<input type="hidden" name="firstname" id="hiddenid" value="<?php echo $db_firstName ?>" />
+					 <tr>
+                        <td>Date :</td>
+                       <td><input type="date" id="date" name="date" required></td>
+                        </tr>
+					 <tr>
+                        <td>Time : </td>
+                        <td><input type="time" name="time" required></td>
+                    </tr>
+                    
+                    
+                </tbody>
+                </table>
 
-					<input class="submit-button" type="submit" id="submit" name="submit" value="Schedule Interview" />
-            </form>
-            
-
+					 <button type="submit" name="save" class="site-btn text-white">Schedule Interview</button>
+</form>					
         </section>
-
 <script>
+var dt= new Date();
+   var yyyy = dt.getFullYear().toString();
+   var mm = (dt.getMonth()+1).toString(); // getMonth() is zero-based
+   var dd  = dt.getDate().toString();
+   var min = yyyy +'-'+ (mm[1]?mm:"0"+mm[0]) +'-'+ (dd[1]?dd:"0"+dd[0]); // padding
 
-
+$('#date').prop('min',min);
 </script>
-
 <?php include ('footer.php'); ?>
