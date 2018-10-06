9738d4d1b60464657d3e1783aee513f62e781a22
AswathyAkhilesh <42207665+AswathyAkhilesh@users.noreply.github.com>
2018-10-07 00:27:13 +0100

    Updated session unset

--- includes/logout.php
+++ includes/logout.php
@@ -1,22 +1,17 @@
-<?php session_start(); ?>
-
-<?php 
-
-$_SESSION['db_id'] = null;
-$_SESSION['db_username'] = null;
-$_SESSION['db_firstName'] = null;
-$_SESSION['db_lastName'] = null;
-$_SESSION['db_userRole'] = null;
-$_SESSION['db_email'] = null;
-$_SESSION['db_dob'] = null;
-$_SESSION['db_phoneNumber'] = null;
-$_SESSION['db_address'] = null;
-$_SESSION['db_city'] = null;
-$_SESSION['db_county'] = null;
-$_SESSION['db_eirCode'] = null;
-$_SESSION['db_country'] = null;
-$_SESSION['db_userRole'] = null;
-
+<?php session_start();
+unset($_SESSION['db_id']);
+unset($_SESSION['db_username']);
+unset($_SESSION['db_firstName']);
+unset($_SESSION['db_lastName']);
+unset($_SESSION['db_userRole']);
+unset($_SESSION['db_email']);
+unset($_SESSION['db_dob']);
+unset($_SESSION['db_phoneNumber']);
+unset($_SESSION['db_address']);
+unset($_SESSION['db_city']);
+unset($_SESSION['db_county']);
+unset($_SESSION['db_eirCode']);
+unset($_SESSION['db_country']);
+unset($_SESSION['db_userRole']);
 header("Location: ../index.php");
-
 ?>
